27a966c6808385c87036e443069d1a531d46797d
Cheever Esler <ames.esler@gmail.com>
2022-10-31 16:40:27 -0300

    added views folder, starting to use res.render

--- express_server.js
+++ express_server.js
@@ -15,9 +15,13 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -29,4 +33,9 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
