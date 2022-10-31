f674c48baf433a96b5c42a8d294cef626db98631
Cheever Esler <ames.esler@gmail.com>
2022-10-31 20:45:14 -0300

    implemented new URL form

--- express_server.js
+++ express_server.js
@@ -40,6 +40,10 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars); // passing template name and variable
 });
 
+app.get('/urls/new', (req, res) => {      // must stay before get /urls/:id
+  res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
