547e03f64edd6420d177a41ce3b262ef1f1583e2
Cheever Esler <ames.esler@gmail.com>
2022-11-02 16:00:36 -0300

    updated server for new registration and cookie tracking

--- express_server.js
+++ express_server.js
@@ -10,12 +10,25 @@ const PORT = 8080;     // using default port 8080
 
 app.set('view engine', 'ejs');
 
-
+// Databases and Tools
 /////////////////////
 
 const generateRandomString = function() {
   const randomString = Math.random().toString(36).replace('0.', ''); // disclaimer: not completely understanding the deep mechanics of .toString(36)
-  return randomString.slice(0, 6); // this needs to be fixed. right now only able to set lower-case letters and 0-9
+  return randomString.slice(0, 6); // this should be changed. right now only able to set lower-case letters and 0-9
+};
+
+const users = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
+  },
+  user2RanodmID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
+  }
 };
 
 const urlDatabase = {
@@ -23,7 +36,8 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-
+//////////// Listener
+/////////////////////
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -32,89 +46,133 @@ app.listen(PORT, () => {
 
 app.use(express.urlencoded({ extended: true })); // POST-related body-parser, must stay before all other routing
 
-/////////////// Login
+/////////////// Routing
 /////////////////////
+
+// Login
 
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
+  res.cookie('user_id', req.body.email);
+
   res.redirect('/urls');
 });
 
-////////////// Logout
-/////////////////////
+// Logout
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
-})
+  res.clearCookie('user_id');
 
-
-// home
-app.get('/', (req, res) => {
   res.redirect('/urls');
 });
 
-// test
+// Register
+
+app.get('/register', (req, res) => {
+  const id = req.cookies['user_id'];
+  const templateVars = {
+    user: users[id]
+  };
+
+  res.render('user_register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  const newUserID = generateRandomString();
+  const newEmail = req.body.email;
+  const newPassword = req.body.password;
+
+  users[newUserID] = {
+    id: newUserID,
+    email: newEmail,
+    password: newPassword
+  };
+  res.cookie('user_id', newUserID);
+
+  res.redirect('urls');
+});
+
+// home
+
+app.get('/', (req, res) => {
+
+  res.redirect('/urls');
+});
+
+// testing
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-// test
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 // browse
+
 app.get('/urls', (req, res) => {
+  const id = req.cookies['user_id'];
   const templateVars = {
-    username: req.cookies['username'],
+    user: users[id],
     urls: urlDatabase
   };
+
   res.render('urls_index', templateVars); // passing template name and variable
 });
 
-// add
+// add (post)
+
 app.post('/urls', (req, res) => {         // POST FORM for new URLs
-  console.log(req.body); // Log the POST request body to the console
   const randomURL = generateRandomString();
   urlDatabase[randomURL] = req.body.longURL;
+
   res.redirect(`/urls/${randomURL}`); // temporary: respond with 'Ok'
 });
 
-// read/add
 app.get('/urls/new', (req, res) => {      // must stay before get /urls/:id
+  const id = req.cookies['user_id'];
   const templateVars = {
-    username: req.cookies['username']
+    user: users[id]
   };
+
   res.render('urls_new', templateVars);
 });
 
 // read
+
 app.get('/urls/:id', (req, res) => {
+  const id = req.cookies['user_id'];
   const templateVars = {
-    username: req.cookies['username'],
+    user: users[id],
     id: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
+
   res.render('urls_show', templateVars);
 });
 
 //edit
+
 app.post('/urls/:id', (req, res) => {
   const urlID = req.params.id;
   urlDatabase[urlID] = req.body.longURL;
+
   res.redirect(`/urls`); // can we redirect to same page though? this seems less functional
 });
 
 // delete
+
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
+
   res.redirect('/urls');
 });
 
-// short redirect
+// shortened redirect
+
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
+
   res.redirect(longURL);
 });
 
