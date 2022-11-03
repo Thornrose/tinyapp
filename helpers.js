08a8393dc4131c27a17c69c4eb41ad98748301d9
Cheever Esler <ames.esler@gmail.com>
2022-11-03 20:11:16 -0300

    migrated more helper functions

--- helpers.js
+++ helpers.js
@@ -1,5 +1,8 @@
 
-
+const generateRandomString = function() {
+  const randomString = Math.random().toString(36).replace('0.', ''); // disclaimer: not completely understanding the deep mechanics of .toString(36)
+  return randomString.slice(0, 6); // this should be changed. right now only able to set lower-case letters and 0-9
+};
 
 const getUserByEmail = function(testEmail, database) {
   for (const user in database) {
@@ -10,5 +13,29 @@ const getUserByEmail = function(testEmail, database) {
   return undefined;
 };
 
+const urlsForUser = function(id, database) {
+  const userURLs = {};
+  for (const urlID in database) {
+    if (database[urlID].userID === id) {
+      userURLs[urlID] = database[urlID];
+    }
+  }
+  return userURLs;
+};
 
-module.exports = { getUserByEmail };
+const urlChecker = function(urlToCheck, database) {
+
+  for (const url in database) {
+    if (urlToCheck === url) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+  urlChecker
+};
